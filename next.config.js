7adf0283fd10ef80edb97bc239db8a40037bd0af
gayatri9213 <gayatri.sonar@aressindia.net>
2024-11-19 12:41:55 +0530

    Adding web.config file

--- next.config.js
+++ next.config.js
@@ -10,10 +10,8 @@ const nextConfig = {
     REACT_APP_API_BASE_URL: process.env.REACT_APP_API_BASE_URL,
     REACT_APP_API_CHAT_URL: process.env.REACT_APP_API_CHAT_URL,
     REACT_APP_INJECT_API_BASE_URL: process.env.REACT_APP_INJECT_API_BASE_URL,
-    REACT_APP_VECTORDB_API_BASE_URL:
-      process.env.REACT_APP_VECTORDB_API_BASE_URL,
-  },
-  
+    REACT_APP_VECTORDB_API_BASE_URL: process.env.REACT_APP_VECTORDB_API_BASE_URL,
+  },  
 };
 
 module.exports = nextConfig;
