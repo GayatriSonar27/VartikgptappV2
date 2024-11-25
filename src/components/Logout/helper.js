3daa3ec994d453bf79c7ed9373e60a9e978f70db
gayatri9213 <gayatri.sonar@aressindia.net>
2024-11-25 17:45:25 +0530

    back button prevention

--- src/components/Logout/helper.js
+++ src/components/Logout/helper.js
@@ -1,14 +1,29 @@
 import { useMsal } from "@azure/msal-react";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export function useLogout() {
   const { instance } = useMsal();
   const router = useRouter();
+
   const logout = () => {
     localStorage.removeItem("formData");
     instance.logoutPopup().then(() => {
-      router.push("/");
+      router.push("/").then(() => {
+        window.history.pushState(null, "", window.location.href); 
+        window.onpopstate = () => {
+          window.history.pushState(null, "", window.location.href);
+        };
+      });
     });
   };
+
+  useEffect(() => {
+    window.history.pushState(null, "", window.location.href);
+    window.onpopstate = () => {
+      window.history.pushState(null, "", window.location.href);
+    };
+  }, []);
+
   return logout;
 }
